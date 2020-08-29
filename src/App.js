057ee7d30aa02996f2b49fffd1742c2d0a1a0de1
heej <sisesu92@gmail.com>
2020-08-29 21:37:11 +0900

    namespacing api routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  // request from the frontend to the backend
+  // fetch('/time') will make the request go to http://localhost:3000/time
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +28,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}</p>
       </header>
     </div>
   );
