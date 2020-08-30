ccc31f4f6fce1cfa849dd9d56d8f6a09d3b4e7a8
heej <sisesu92@gmail.com>
2020-08-31 03:41:38 +0900

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
-  // request from the frontend to the backend
-  // fetch('/time') will make the request go to http://localhost:3000/time
   useEffect(() => {
     fetch('/api/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
@@ -16,19 +15,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}</p>
+        <BrowserRouter>
+          <div>
+            <Link className="App-link" to="/">Home</Link>
+            &nbsp;|&nbsp;
+            <Link className="App-link" to="/page2">Page2</Link>
+          </div>
+          <Switch>
+            <Route exact path="/">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+                <p>The current time is {currentTime}.</p>
+            </Route>
+            <Route path="/page2">
+                <p>This is page 2!</p>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
